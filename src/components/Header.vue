75a38f36db1173982954f9edc958868340406e13
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-05-28 17:44:39 +0900

    [Fix] ページ内移動

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,19 +2,19 @@
   <header id="header" class="fixed-top" :class="{ 'header-scrolled': scrollY > 100 }">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><RouterLink to="/">Hochste</RouterLink></h1>
+      <h1 class="logo"><a href="/">Hochste</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><RouterLink class="nav-link" to="#top">Top</RouterLink></li>
-          <li><RouterLink class="nav-link" to="#about">About</RouterLink></li>
-          <li><RouterLink class="nav-link" to="#services">Services</RouterLink></li>
-          <li><RouterLink class="nav-link" to="#admin">Admin</RouterLink></li>
-          <li><RouterLink class="nav-link" to="#faq">FAQ</RouterLink></li>
-          <li><RouterLink class="nav-link" to="#contact">Contact</RouterLink></li>
-          <li><RouterLink class="login" to="#">Login</RouterLink></li>
+          <li><a class="nav-link" href="#top">Top</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#services">Services</a></li>
+          <li><a class="nav-link" href="#admin">Admin</a></li>
+          <li><a class="nav-link" href="#faq">FAQ</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+          <li><a class="login" href="#">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
