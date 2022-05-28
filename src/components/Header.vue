402192e2b5438f8720e49ea27b2d30fea965c4ba
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-05-28 16:56:19 +0900

    [Add] いろいろ

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,28 @@
+<template>
+  <header id="header" class="fixed-top" :class="{ 'header-scrolled': scrollY > 100 }">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><RouterLink to="/">Hochste</RouterLink></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><RouterLink class="nav-link" to="#top">Top</RouterLink></li>
+          <li><RouterLink class="nav-link" to="#about">About</RouterLink></li>
+          <li><RouterLink class="nav-link" to="#services">Services</RouterLink></li>
+          <li><RouterLink class="nav-link" to="#admin">Admin</RouterLink></li>
+          <li><RouterLink class="nav-link" to="#faq">FAQ</RouterLink></li>
+          <li><RouterLink class="nav-link" to="#contact">Contact</RouterLink></li>
+          <li><RouterLink class="login" to="#">Login</RouterLink></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+</template>
+
 <script>
 export default {
   data() {
@@ -15,28 +40,3 @@ export default {
   }
 }
 </script>
-
-<template>
-  <header id="header" class="fixed-top" :class="{ 'header-scrolled': scrollY > 90 }">
-    <div class="container d-flex align-items-center justify-content-between">
-
-      <h1 class="logo"><RouterLink to="/">Hochste</RouterLink></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Login</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-
-    </div>
-  </header>
-</template>
---
+++ src/components/admin/AdminItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+    <div class="member">
+      <div class="member-img">
+        <img :src="icon" class="img-fluid" alt="">
+        <div class="social">
+          <slot name="social" />
+        </div>
+      </div>
+      <div class="member-info">
+        <h4>{{ name }}</h4>
+        <span>{{ role }}</span>
+       </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MemberItem",
+  props: {
+    icon: String,
+    name: String,
+    role: String
+  }
+}
+</script>
+
